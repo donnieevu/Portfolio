a8e7f612cd15fa4ef400d553d9cd1704a91ced93
Donald Vu <donaldhdvu@gmail.com>
2025-04-30 20:49:10 -0500

    deleting images and changing img src to right location

--- portfolio_webpage/app/page.tsx
+++ portfolio_webpage/app/page.tsx
@@ -84,7 +84,7 @@ export default function Portfolio() {
           <div className="flex-1 flex justify-center md:justify-end">
             <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-primary/20">
               <img
-                src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/donald-headshot.jpg-PM9ekRsT8zfebNht4UEUZerP9QTsPP.jpeg"
+                src="/images/donald-headshot.png"
                 alt="Donald Vu"
                 className="object-cover w-full h-full"
               />
